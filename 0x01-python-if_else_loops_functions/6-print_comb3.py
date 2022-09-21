6d8cd1193dc233b4e75735921ea36972e1bcfa2a
KauffmannAko <nanakauffmann@gmqil.com>
2022-09-21 21:35:32 +0100

    done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 for i in range(0, 9):
-    for l in range(i+1, 10):
+    for num in range(i+1, 10):
         if i == 8:
-            print("{0:d}{1:d}".format(i, l))
+            print("{0:d}{1:d}".format(i, num))
             break
-        print("{0:d}{1:d}".format(i, l), end=", ")
+        print("{0:d}{1:d}".format(i, num), end=", ")
