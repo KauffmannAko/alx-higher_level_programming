f296f73ff966337f343a4e38b665aa07297183e6
KauffmannAko <nanakauffmann@gmqil.com>
2022-10-13 20:18:34 +0100

    don

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thur Oct 13 19:22:54 2022
+@author: Robinson Montes
+"""
+
+
+class Square:
+    """Square is a an empty class
+
+    Attributes:
+        size (int): The size of the square
+    """
+
+
+    def __init__(self, size):
+        """The __init__ method for Square class
+
+        Args:
+            size: (:obj:'int'): A private instance size
+        """
+        self.__size = size
