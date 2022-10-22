29de2a9df0334d1b5b94d6ddda1047d3d92da3c4
KauffmannAko <nanakauffmann@gmqil.com>
2022-10-22 17:30:07 +0100

    done

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -74,4 +74,13 @@ class Rectangle:
         else:
             return 2 * (self.width + self.height)
 
-
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_2.area() > rect_1.area():
+            return rect_2
+        else:
+            return rect_1
