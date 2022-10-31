b80622cff6d76a252f9134de18c557f3a8c8bf8e
KauffmannAko <nanakauffmann@gmqil.com>
2022-10-31 20:01:06 +0100

    corrected

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon. Oct. 31,2022
+@author: Nana Kauffmann
+"""
+
+
 def lookup(obj):
     """Return the attributes and methods of an object
     """
