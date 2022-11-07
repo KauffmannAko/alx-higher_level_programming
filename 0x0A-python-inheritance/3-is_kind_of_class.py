58f1df8036155c71718957f9a105189e134c9fc8
KauffmannAko <nanakauffmann@gmqil.com>
2022-11-07 09:17:42 +0100

    done

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon. Oct. 31,2022
+@author: Nana Kauffmann
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
