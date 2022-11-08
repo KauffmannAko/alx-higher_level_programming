89ec767a4cf39a053ef882f9e7b16881502b3ec7
KauffmannAko <nanakauffmann@gmqil.com>
2022-11-08 09:14:19 +0100

    done

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,4 +15,3 @@ def inherits_from(obj, a_class):
     if not isinstance(a_class, type):
         raise TypeError("a_class type must be type")
     return (issubclass(type(obj), a_class) and type(obj) is not a_class)
-    
