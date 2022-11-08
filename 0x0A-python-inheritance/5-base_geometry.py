70c2535d5addb8e349bfd126e6503fd6e97260c1
KauffmannAko <nanakauffmann@gmqil.com>
2022-11-08 09:36:58 +0100

    don

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon. Oct. 31,2022
+@author: Nana Kauffmann
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class
+    """
+    pass
