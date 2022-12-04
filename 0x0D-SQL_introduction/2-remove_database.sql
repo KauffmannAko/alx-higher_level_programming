04384dca1bad00b9fdf433b2af909efcce9aad07
KauffmannAko <nanakauffmann@gmqil.com>
2022-12-04 23:12:07 +0100

    done

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
--- Create databases using CREATE
--- Create database hbtn_0c_0 in  MySQL server
-DROP DATABASE IF EXITS hbtn_0c_0
+-- Delete databases using DROP
+-- Delete database hbtn_0c_0 in  MySQL server
+DROP DATABASE IF EXITS hbtn_0c_0;
