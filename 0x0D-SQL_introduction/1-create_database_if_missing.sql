ab15897c468bcc451dd546a2f1fd9b5fe0694258
KauffmannAko <nanakauffmann@gmqil.com>
2022-12-04 23:01:29 +0100

    done

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
--- Create databases using CREATE
--- Create database hbtn_0c_0 in  MySQL server
-CREATE DATABASE IF Not EXITS hbtn_0c_0;
+-- Create database using CREATE
+-- Creates database hbtn_0c_0 in MySQL
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
