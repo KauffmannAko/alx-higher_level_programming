536af2bdee2bea7b415688cceadea795f0121f1b
KauffmannAko <nanakauffmann@gmqil.com>
2023-02-23 21:07:47 +0100

    done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,6 +74,16 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print(" ")
+    def __str__(self):
+        """str method for class Rectangle
+
+        Returns:
+            str: he string: [class_name] (id) x/y - width/height
+        """
+        string = "[{}] ({}) {}/{}".format(self.__class__.__name__, 
+                                          self.id,self.__x,self.__y, 
+                                          self.__width, self.__height)
+        return string
 
     @property
     def width(self):
